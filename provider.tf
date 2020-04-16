3ae7bc9228a6a3b3ae681e2d7ff37e47707afd0a
Venu <46160810+naikvenu@users.noreply.github.com>
2020-04-16 22:11:51 +1000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,19 +6,3 @@ provider "oci" {
   region           = "${var.region}"
 }
 
-terraform {
-  backend "s3" {
-    bucket   = "terraform-test"
-    key      = "terraform.tfstate"
-    region   = "ap-sydney-1"
-    endpoint = "https://ociateam.compat.objectstorage.ap-sydney-1.oraclecloud.com"
-    
-
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    skip_requesting_account_id  = true
-    skip_get_ec2_platforms      = true
-    skip_metadata_api_check     = true
-    force_path_style            = true
-  }
-}
